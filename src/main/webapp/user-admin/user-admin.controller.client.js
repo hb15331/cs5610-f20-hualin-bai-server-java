5620bc8ff80ddc7ad4829175f4146bdda0d6f2b4
hb15331 <hb15331@outlook.com>
2020-09-25 11:10:04 -0400

    first commit for user admin controller

--- src/main/webapp/user-admin/user-admin.controller.client.js
+++ src/main/webapp/user-admin/user-admin.controller.client.js
@@ -8,12 +8,14 @@ let users = [
     {
         username: 'bob',
         fName: 'Robert',
-        lName: 'Marley'
+        lName: 'Marley',
+        role: 'Student'
     },
     {
         username: 'charlie',
         fName: 'Charlie',
-        lName: 'Garcia'
+        lName: 'Garcia',
+        role: 'Admin'
     }
 ]
 
@@ -22,7 +24,8 @@ let tbody
 let template
 let clone
 let $createBtn
-let $usernameFld, $firstNameFld
+let $usernameFld, $firstNameFld, $lastNameFld
+let $roleFld
 
 
 const deleteUser1 = (event) => {
@@ -31,56 +34,64 @@ const deleteUser1 = (event) => {
 }
 
 const renderUsers = (users) => {
-
+    // remove the redundant current content from table
+    // otherwise would add modified user data to current data from last update
     tbody.empty()
-    for(let i=0; i < users.length; i++) {
-        const user = users[i]
 
+    for(let i = 0; i < users.length; i++) {
+        const user = users[i]
+        // template row contains some dummy data
         clone = template.clone()
         clone.removeClass("wbdv-hidden")
 
+        // overwrite the content of cloned row with array data
         clone.find(".wbdv-username").html(user.username)
         clone.find(".wbdv-first-name").html(user.fName)
+        clone.find(".wbdv-last-name").html(user.lName)
+        clone.find(".wbdv-role").html(user.role)
+        // assign a click event to the remove icon on i-th row
         clone.find(".wbdv-remove").click(() => deleteUser2(i))
 
         tbody.append(clone)
     }
 }
 
-
+// modify the user array and reflect the change in data model to DOM
 const deleteUser2 = (index) => {
     users.splice(index, 1)
+    // re-render the html after deleting the i-th object in user array
     renderUsers(users)
 }
 
 
 const createUser = () => {
-    console.log("create user")
+    // retrieve the value of input field
     const username = $usernameFld.val()
     const firstName = $firstNameFld.val()
+    const lastName = $lastNameFld.val()
+    const role = $roleFld.val()
+    console.log(role)
 
+    // reset the value of input field
     $usernameFld.val("")
     $firstNameFld.val("")
+    $lastNameFld.val("")
+    $roleFld.val("Faculty")
 
+    // add this new user object to user array
     const newUser = {
         username: username,
         fName: firstName,
-        lName: 'TBD1',
-        role: 'TBD2'
+        lName: lastName,
+        role: role
     }
+    // modify the data model and re-render the DOM
     users.push(newUser)
     renderUsers(users)
 }
 
 
 const init = () => {
-    const heading1 = jQuery("h1")
-    // heading1
-    //     .css("color", "yellow")
-    //     .css("backgroundColor", "red")
-    //     .html("User Administrator")
-    //     .append(" - Only for Administrators")
-    //     .append("<button>Ok</button>")
 
     const container = $(".container")
     tbody = $("tbody")
@@ -88,6 +99,8 @@ const init = () => {
     $createBtn = $(".wbdv-create").click(createUser)
     $firstNameFld = $("#firstNameFld")
     $usernameFld = $("#usernameFld")
+    $lastNameFld = $("#lastNameFld")
+    $roleFld = $("#roleFld")
 
     renderUsers(users)
 }
