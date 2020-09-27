7be8811b31298238b10e0d419ce2e7f6a7dffc82
hb15331 <hb15331@outlook.com>
2020-09-26 23:32:01 -0400

    first commit for controller and service

---
+++ src/main/webapp/services/user.service.client.js
@@ -0,0 +1,55 @@
+function AdminUserServiceClient() {
+    this.createUser = createUser;
+    this.findAllUsers = findAllUsers;
+    this.findUserById = findUserById;
+    this.deleteUser = deleteUser;
+    this.updateUser = updateUser;
+    this.url = 'https://wbdv-generic-server.herokuapp.com/api/jannunzi/users';
+    // var self = this;
+
+    // lambda functions that return a promise on calling json()
+    // then manipulate the json data in the controller
+    function findAllUsers() {
+        return fetch(this.url)
+            .then(response => response.json());
+    }
+
+    // user object is formatted by browser as a stream of bytes
+    // and sent to server
+    function createUser(user) {
+        return fetch(this.url, {
+            method: 'POST',
+            // body: user,
+            // convert jason to a string
+            body: JSON.stringify(user),
+            headers: {'content-type': 'application/json'
+            }
+        })
+            // server will respond with actual object that
+            // was added to server, including some metadata
+            .then(response => response.json());
+    }
+
+    function findUserById(userId) { }
+
+    // params: userId and all new data of user
+    function updateUser(userId, user) {
+        // backtick: embed the variable inside the string
+        return fetch(`https://wbdv-generic-server.herokuapp.com/api/jannunzi/users/${userId}`, {
+            method: 'PUT',
+            body: JSON.stringify(user),
+            headers: {'content-type': 'application/json'
+            }
+        })
+    }
+
+    // userId is the primary key that specifies which record to remove
+    // override the default method GET with DELETE
+    function deleteUser(userId) {
+        return fetch(this.url + userId, {
+            method: 'DELETE'
+        });
+    }
+
+
+}
--- src/main/webapp/user-admin/user-admin.controller.client.js
+++ src/main/webapp/user-admin/user-admin.controller.client.js
@@ -27,11 +27,25 @@ let $createBtn
 let $usernameFld, $firstNameFld, $lastNameFld
 let $roleFld
 
+const userService = new AdminUserServiceClient();
 
 const deleteUser1 = (event) => {
     const deleteBtn = $(event.currentTarget)
     deleteBtn.parents("tr.wbdv-template").remove()
 }
+
+// records which user is selected for update
+let selectedUserIndex = -1 // -1 denotes no user is selected
+const selectUser = (index) => {
+
+    selectedUserIndex = index
+    $usernameFld.val(users[index].username)
+    $firstNameFld.val(users[index].first)
+    $lastNameFld.val(users[index].last)
+    $roleFld.val(users[index].role)
+
+}
+
 
 const renderUsers = (users) => {
     // remove the redundant current content from table
@@ -46,21 +60,38 @@ const renderUsers = (users) => {
 
         // overwrite the content of cloned row with array data
         clone.find(".wbdv-username").html(user.username)
-        clone.find(".wbdv-first-name").html(user.fName)
-        clone.find(".wbdv-last-name").html(user.lName)
+        clone.find(".wbdv-first-name").html(user.first)
+        clone.find(".wbdv-last-name").html(user.last)
         clone.find(".wbdv-role").html(user.role)
         // assign a click event to the remove icon on i-th row
         clone.find(".wbdv-remove").click(() => deleteUser2(i))
+        clone.find(".wbdv-edit").click(() => selectUser(i))
 
         tbody.append(clone)
     }
 }
 
+
 // modify the user array and reflect the change in data model to DOM
-const deleteUser2 = (index) => {
-    users.splice(index, 1)
+const deleteUser2 = (_index) => {
+    // users.splice(index, 1)
     // re-render the html after deleting the i-th object in user array
-    renderUsers(users)
+    // renderUsers(users)
+
+    // use index to get the user from local array that we've clicked on
+    const user = users[_index];
+    // retrieve the id attribute from that user
+    const userId = user._id;
+    // pass id to invoke the service delete
+    // deleteUser returns a promise
+    userService.deleteUser(userId)
+        // after the server successfully deletes the record
+        // removes the same record from local array
+        // can save an extra call to server for latest data
+        .then(response => {
+            users.splice(_index, 1);
+            renderUsers(users);
+        })
 }
 
 
@@ -78,22 +109,58 @@ const createUser = () => {
     $lastNameFld.val("")
     $roleFld.val("Faculty")
 
-    // add this new user object to user array
+    // newUser: the user object sent to remote server
+    // this object does not have an id
     const newUser = {
         username: username,
-        fName: firstName,
-        lName: lastName,
+        first: firstName,
+        last: lastName,
         role: role
     }
+
+    // actualNewUser: the user object stored in the database
+    // the actual user received from server has id
+    // that can be used for delete, and it is pushed to local array
+    userService.createUser(newUser).then(actualNewUser => {
+        users.push(actualNewUser);
+        renderUsers(users);
+    })
     // modify the data model and re-render the DOM
-    users.push(newUser)
-    renderUsers(users)
+    // users.push(newUser)
+    // renderUsers(users)
 }
+
+const updateUser = () => {
+
+    const selectedUser = users[selectedUserIndex];
+    // selectedUser.username = $usernameFld.val();
+    // selectedUser.first = $firstNameFld.val();
+    // selectedUser.last = $lastNameFld.val();
+
+    const newUserName = $usernameFld.val();
+    const newFirstName = $firstNameFld.val();
+    const newLastName = $lastNameFld.val();
+
+    const updatedUser = {
+        username: newUserName,
+        first: newFirstName,
+        last: newLastName
+    }
+    // modify the local array after the promise is fulfilled
+    userService.updateUser(selectedUser._id, updatedUser)
+        .then(() => {
+            users[selectedUserIndex] = updatedUser;
+            renderUsers(users);
+        })
+
+}
+
 
 
 const init = () => {
 
-    const container = $(".container")
+    userService.findAllUsers();
+
     tbody = $("tbody")
     template = $("tr.wbdv-template")
     $createBtn = $(".wbdv-create").click(createUser)
@@ -102,7 +169,18 @@ const init = () => {
     $lastNameFld = $("#lastNameFld")
     $roleFld = $("#roleFld")
 
-    renderUsers(users)
+    $(".wbdv-update").click(updateUser);
+
+    // _users: this array is from the server
+    // users: local copy of the remote array
+    // render the page based on the local copy of users
+    userService.findAllUsers()
+        .then((_users) => {
+            console.log(_users)
+            users = _users
+            renderUsers(users)
+        })
+
 }
 
 $(init)
